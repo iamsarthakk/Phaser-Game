c238065b60ea2a243409e4e27cb2e6d12b7b990a
iamsarthakk <sarthakkumar160627@gmail.com>
2020-10-02 16:11:03 +0530

    Update game view

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -13,7 +13,7 @@ var Menu = {
     create: function () {
         // Add a sprite to your game, here the sprite will be the game's logo
         // Parameters are : X , Y , image name (see above)
-        this.sky = this.add.sprite(320, 240, 'sky').setInteractive();
+        this.sky = this.add.sprite(400, 300, 'sky').setInteractive();
         this.sky.on('pointerdown', () => this.scene.start('Game'));
         // this.add.button(0, 0, 'sky', this.startGame, this);
         introText = this.add.text(24, 8, "Clear all the baddies when they're weak!  \nClick to start playing, use the keyboard to move. \n\n(Click the white area around the game to make movement work)"
